cf5a90fc707b0ebe1a0ef08cbf2e2fc4f92ba061
knangcas <knangcas@asu.edu>
2025-02-03 10:00:06 -0600

    first commit

---
+++ src/main/java/com/example/demo1spring/Demo1springApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo1spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1springApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1springApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demo1spring/model/Course.java
@@ -0,0 +1,2 @@
+package com.example.demo1spring.model;public class Course {
+}
