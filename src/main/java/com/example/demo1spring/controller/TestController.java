920fb454f46a5bbb5281dcc137604c28e27c42d1
knangcas <knangcas@asu.edu>
2025-02-03 10:00:23 -0600

    second commit

--- src/main/java/com/example/demo1spring/controller/TestController.java
+++ src/main/java/com/example/demo1spring/controller/TestController.java
@@ -1,2 +1,26 @@
-package com.example.demo1spring.controller;public class TestController {
+package com.example.demo1spring.controller;
+
+
+import com.example.demo1spring.model.Course;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TestController {
+
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "in28minutes"),
+                new Course(2, "Learn DevOps", "in28minutes")
+        );
+
+    }
+
+
 }
--- src/main/java/com/example/demo1spring/model/Course.java
+++ src/main/java/com/example/demo1spring/model/Course.java
@@ -1,2 +1,39 @@
-package com.example.demo1spring.model;public class Course {
+package com.example.demo1spring.model;
+
+
+
+public class Course {
+
+
+    private long id;
+    private String name;
+
+    private String author;
+
+    public Course(long id, String name, String author) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
 }
